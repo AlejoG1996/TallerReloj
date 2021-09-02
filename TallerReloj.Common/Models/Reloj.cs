3cbd4f65db5fc8e8fc4e77cabc2296d2f09363e2
AlejoG1996 <galeanomadrigalalejnadro@gmail.com>
2021-09-02 16:00:03 -0500

    Add Entiry

---
+++ TallerReloj.Common/Models/Reloj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TallerReloj.Common.Models
+{
+    public class Reloj
+    {
+        public int IdEmpleado { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public DateTime Hora { get; set; }
+        public int Tipo { get; set; }
+
+        public bool Consolidado { get; set; }
+    }
+}
---
+++ TallerReloj.Function/Entities/RelojEntity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TallerReloj.Function.Entities
+{
+    public class RelojEntity: TableEntity
+    {
+        public int IdEmpleado { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public DateTime Hora { get; set; }
+        public int Tipo { get; set; }
+
+        public bool Consolidado { get; set; }
+    }
+}
